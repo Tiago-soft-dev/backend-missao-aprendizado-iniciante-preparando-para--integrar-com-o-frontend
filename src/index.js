5dfe22796ee7f84bea32d7e39c17b84a7bdaea7f
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-07-25 12:46:44 -0300

    implementação de erros inesperados

--- src/index.js
+++ src/index.js
@@ -17,10 +17,11 @@ const app=express()
 //middlewares
 app.use(express.json()) //express usando json
 app.use(cors())
-
-
 app.use('/personagem', personagemRouter)
-
+app.use(function(err,req,res,next){
+    console.error(err.stack)
+    res.status(500).send({erro: 'Algo deu errado!'})
+})
 app.listen(3000, ()=>console.log('servidor online'))
 }
 main()
